55c0ccafe7893f006a0ecf899c08a8fcc2046b34
pbang <phillipbang@gmail.com>
2018-11-29 21:27:21 -0800

    testing connection

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,10 @@
 var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js")
+
+// Render index
+router.get('/', function(req, res) {
+    res.render('index');
+})
+
+module.exports = router;
