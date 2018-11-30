f74548225fdacdda96bfc40bb32ecedf0c26fef8
pbang <phillipbang@gmail.com>
2018-11-29 19:27:55 -0800

    add seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDouble', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McChicken', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', FALSE);
