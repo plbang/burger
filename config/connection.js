d034d057f5e6e980f0beb5fc4550d6bbe3bd25e1
pbang <phillipbang@gmail.com>
2018-11-30 01:30:39 -0800

    fuck

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : '127.0.0.1',
     port     : 3306, 
     user     : 'root',
     password : '',
     database : 'burgers_db',
-    socketPath:'/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
 connection.connect(function(err) {
